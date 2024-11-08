9a6743bd75e7d5b9815aa80cd96a6f74553dd059
IbnuZakyFauzi <izaky.thb@gmail.com>
2024-11-08 19:09:16 +0700

    CS8

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -15,13 +13,17 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 stopFriction;
     private Rigidbody2D rb;
 
+    private float xLimitRight = 8.7f;   
+    private float xLimitLeft = -8.7f;   
+    private float yLimitTop = 4.49f;    
+    private float yLimitBottom = -5f;   
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
         moveVelocity = 2 * maxSpeed / timeToFullSpeed;
-        moveFriction = -2 * maxSpeed / (Mathf.Pow(timeToFullSpeed.magnitude, 2));
-        stopFriction = -2 * maxSpeed / (Mathf.Pow(timeToStop.magnitude, 2));
+        moveFriction = -2 * maxSpeed / (timeToFullSpeed * timeToFullSpeed);
+        stopFriction = -2 * maxSpeed / (timeToStop * timeToStop);
     }
 
     void Update()
@@ -32,53 +34,21 @@ public class PlayerMovement : MonoBehaviour
     public void Move()
     {
         moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+        rb.velocity = moveDirection * maxSpeed;
 
-        if (moveDirection != Vector2.zero)
-        {
-            Vector2 targetVelocity = moveDirection * maxSpeed;
-            rb.velocity = Vector2.MoveTowards(rb.velocity, targetVelocity, moveVelocity.magnitude * Time.deltaTime);
-        }
-        else
-        {
-            Vector2 frictionForce = GetFriction();
-            rb.velocity = Vector2.MoveTowards(rb.velocity, Vector2.zero, frictionForce.magnitude * Time.deltaTime);
+        // Membatasi posisi pemain agar tetap dalam area yang diinginkan
+        Vector3 clampedPosition = rb.position;
+        clampedPosition.x = Mathf.Clamp(clampedPosition.x, xLimitLeft, xLimitRight);  // Membatasi posisi kiri dan kanan
+        clampedPosition.y = Mathf.Clamp(clampedPosition.y, yLimitBottom, yLimitTop);  // Membatasi posisi atas dan bawah
 
-            if (rb.velocity.magnitude < stopClamp.magnitude)
-            {
-                rb.velocity = Vector2.zero;
-            }
-        }
-
-        rb.velocity = new Vector2(
-            Mathf.Clamp(rb.velocity.x, -maxSpeed.x, maxSpeed.x),
-            Mathf.Clamp(rb.velocity.y, -maxSpeed.y, maxSpeed.y)
-        );
+        rb.position = clampedPosition;
     }
 
-    public Vector2 GetFriction()
-    {
-        if (moveDirection != Vector2.zero)
-        {
-            return new Vector2(
-                moveDirection.x != 0 ? moveFriction.x : 0,
-                moveDirection.y != 0 ? moveFriction.y : 0
-            );
-        }
-        else
-        {
-            return new Vector2(
-                rb.velocity.x != 0 ? stopFriction.x : 0,
-                rb.velocity.y != 0 ? stopFriction.y : 0
-            );
-        }
+    private Vector2 GetFriction(){
+        return rb.velocity.magnitude > 0 ? stopFriction : Vector2.zero;
     }
 
-    public void MoveBound()
-    {
-    }
-
-    public bool IsMoving()
-    {
-        return rb.velocity != Vector2.zero;
+    public bool IsMoving(){
+        return rb.velocity.magnitude > 0.1f;
     }
 }
